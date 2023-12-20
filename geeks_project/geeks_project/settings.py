0c503c3130a6e389c286d70bc9104fdee196f5d6
Num1ss <krutojmer76@gmail.com>
2023-12-21 00:15:11 +0600

    Upload hw3

--- geeks_project/geeks_project/settings.py
+++ geeks_project/geeks_project/settings.py
@@ -113,6 +113,7 @@ STATIC_URL = 'static/'
 STATIC_FILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
