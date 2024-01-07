47bf876d1d808c14061f90bb9ea7609ce372261b
Num1ss <krutojmer76@gmail.com>
2024-01-07 20:04:21 +0600

    hw_5 load

--- geeks_project/tv_shows/urls.py
+++ geeks_project/tv_shows/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'shows'
 urlpatterns = [
-    path('', views.get_shows, name='show_list'),
-    path('show_list/<int:id>/', views.get_show, name='show'),
-    path('create_show/', views.show_create_view, name='create_show'),
+    path('', views.ShowView.as_view(), name='show_list'),
+    path('show_list/<int:id>/', views.GetShow.as_view(), name='show'),
+    path('create_show/', views.ShowCreateView.as_view(), name='create_show'),
     path('delete_show/', views.show_delete_view, name='delete_show'),
-    path('drop_show/<int:id>/delete', views.show_drop_view, name='drop_show'),
+    path('drop_show/<int:id>/delete', views.ShowDropView.as_view(), name='drop_show'),
     path('update_show/', views.show_edit_view, name='edit_show'),
-    path('edit_show/<int:id>/edit', views.show_update, name='update_show')
+    path('edit_show/<int:id>/edit', views.ShowUpdateView.as_view(), name='update_show'),
+    path('search/', views.SearchView.as_view(), name='search')
 ]
--- geeks_project/tv_shows/views.py
+++ geeks_project/tv_shows/views.py
@@ -1,32 +1,49 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
 from . import models, forms
 from django.urls import reverse
 from django.http import HttpResponse
 
-# Read.
-def get_shows(request):
-    if request.method == "GET":
-        shows = models.Show.objects.all()
-        return render(request, template_name='shows/show_list.html',
-                      context={'shows': shows})
+class ShowView(generic.ListView):
+    template_name = 'shows/show_list.html'
+    model = models.Show
 
-def get_show(request, id):
-    show = get_object_or_404(models.Show, id=id)
-    return render(request, template_name='shows/show.html',
-                  context={'show': show})
+    def get_queryset(self):
+        return self.model.objects.all()
 
-# Post.
-def show_create_view(request):
-    if request.method == "POST":
-        form = forms.ShowForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('/'))
+class GetShow(generic.DeleteView):
+    template_name = 'shows/show.html'
+    context_object_name = 'show_id'
+    model = models.Show
 
-    else:
-        form = forms.ShowForm()
-        return render(request, template_name='crud/create_show.html',
-                      context={'form': form})
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(self.model, id=show_id)
+
+
+class ShowCreateView(generic.CreateView):
+    template_name = 'crud/create_show.html'
+    form_class = forms.ShowForm
+    model = models.Show
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(ShowCreateView, self).form_valid(form=form)
+
+
+# def show_create_view(request):
+#     if request.method == "POST":
+#         form = forms.ShowForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse('/'))
+
+#     else:
+#         form = forms.ShowForm()
+#         return render(request, template_name='crud/create_show.html',
+#                       context={'form': form})
 
 
 # Delete.
@@ -36,11 +53,19 @@ def show_delete_view(request):
         return render(request, template_name='crud/delete/delete_show.html',
                       context={'shows_delete': shows})
 
-def show_drop_view(request, id):
-    show_id = get_object_or_404(models.Show, id=id)
-    show_id.delete()
-    # return HttpResponse('Success')
-    return redirect(reverse('delete_show'))
+class ShowDropView(generic.DeleteView):
+    template_name = 'crud/delete/confirm_delete.html'
+    success_url = '/delete_show/'
+
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(models.Show, id=show_id)
+
+# def show_drop_view(request, id):
+#     show_id = get_object_or_404(models.Show, id=id)
+#     show_id.delete()
+#     # return HttpResponse('Success')
+#     return redirect(reverse('delete_show'))
 
 
 # Edit.
@@ -51,18 +76,45 @@ def show_edit_view(request):
                       context={'shows_update': shows})
 
 
-def show_update(request, id):
-    show_id = get_object_or_404(models.Show, id=id)
-    if request.method == 'POST':
-        form = forms.ShowForm(instance=show_id, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('update_show'))
+class ShowUpdateView(generic.UpdateView):
+    template_name = 'crud/update/edit_show.html'
+    form_class = forms.ShowForm
+    success_url = '/update_show/'
 
-    else:
-        form = forms.ShowForm(instance=show_id)
-        return render(request, template_name='crud/update/edit_show.html',
-                      context={
-                          "form": form,
-                          "show_id": show_id,
-                      })
+
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(models.Show, id=show_id)
+    
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(ShowUpdateView, self).form_valid(form=form)
+
+# def show_update(request, id):
+#     show_id = get_object_or_404(models.Show, id=id)
+#     if request.method == 'POST':
+#         form = forms.ShowForm(instance=show_id, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse('update_show'))
+
+#     else:
+#         form = forms.ShowForm(instance=show_id)
+#         return render(request, template_name='crud/update/edit_show.html',
+#                       context={
+#                           "form": form,
+#                           "show_id": show_id,
+#                       })
+    
+
+class SearchView(generic.ListView):
+    template_name = 'shows/show_list.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Show.objects.filter(title__icontains=self.request.GET.get('q'))
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
